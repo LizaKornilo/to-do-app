09ea7aca17786516800109d5d173442c4886cf5d
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2021-11-30 00:35:38 +0300

    feat: #34 add title and logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,8 @@ export function App() {
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(cards));
-  });
+    console.log("use eff");
+  }, [cards]);
 
   return (
     <ThemeContext.Provider value={theme} >
