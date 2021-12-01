ea6ab5e3369c36d751fa1844464e25a524a24253
LizaKornilo <kornilo_ei_18@mf.grsu.by>
2021-12-02 00:00:16 +0300

    feat: #37 add styles to popup

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./popup.css";
+import TextareaAutosize from 'react-textarea-autosize';
 
 function Popup({ trigger, setTrigger, card, updateCard }) {
   const [name, setName] = useState(card.name);
@@ -20,17 +21,23 @@ function Popup({ trigger, setTrigger, card, updateCard }) {
     event.preventDefault();
   }
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+    }
+  }
+
   return trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <button className="close-btn" onClick={() => setTrigger(false)}>
-          &#10006;
+        <button className="popup__close-btn" onClick={() => setTrigger(false)}>
+          +
         </button>
 
         <form className="popup-form" onSubmit={handleSubmit}>
-          <input className='popup__card-name' value={name} onChange={cardNameChange} type='text' placeholder='enter task name...' required />
+          <TextareaAutosize onKeyPress={handleKeyPress} className='popup__card-name' value={name} onChange={cardNameChange} type='text' placeholder='enter task name...' required />
           <div className="popup__is-done" >
-            <input className="is-one" type="checkbox" id="ch" checked={isDone} onChange={isDoneChange} />
+            <input className="is-done" type="checkbox" id="ch" checked={isDone} onChange={isDoneChange} />
             <label htmlFor="ch">is completed</label>
           </div>
           <div className="popup__timestamp">{card.timestamp} </div>
